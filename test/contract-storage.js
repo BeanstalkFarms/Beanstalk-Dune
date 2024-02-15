798ddf8614fbc4ffa0f29a03b294a9230e85d314
Soil King <157099073+soilking@users.noreply.github.com>
2024-02-15 14:51:47 -0800

    Double mapping

--- test/contract-storage.js
+++ test/contract-storage.js
@@ -2,7 +2,7 @@ const ethers = require('ethers');
 const abiCoder = new ethers.AbiCoder();
 const { BigNumber } = require('alchemy-sdk');
 const { providerThenable } = require('../src/provider.js');
-const { BEANSTALK } = require('../src/addresses.js');
+const { BEANSTALK, BEAN, UNRIPE_BEAN, UNRIPE_LP } = require('../src/addresses.js');
 const { storage, types } = require('../src/contracts/beanstalk/storage.json');
 
 function transformMembersList(members) {
@@ -26,8 +26,7 @@ async function makeHandler(contractAddress, blockNumber = 'latest') {
             if (['storageSlot_jslib', 'currentType_jslib'].includes(property)) {
                 return target[property];
             } else {
-                // console.debug(target);
-                // console.debug('Current type:', target.currentType_jslib, '| Seeking property:', property);
+                // console.debug(`Current type: ${target.currentType_jslib}\nCurrent slot: ${target.storageSlot_jslib.toHexString()}\nSeeking property: ${property}`)
                 let returnProxy;
                 let slotOffset = 0;
                 const currentType = types[target.currentType_jslib];
@@ -42,12 +41,13 @@ async function makeHandler(contractAddress, blockNumber = 'latest') {
                 } else if (currentType.hasOwnProperty('value')) {
                     // Mapping
                     returnProxy = new Proxy(types[currentType.value], handler);
-                    const keyType = currentType.key.slice(2); // remove the "t_"
-                    // console.debug('in mapping', keyType, property, target.storageSlot_jslib);
-                    const encoded = abiCoder.encode([keyType, "uint256"], [property, target.storageSlot_jslib.toHexString()]);
+                    let keyType = currentType.key.slice(2); // remove the "t_"
+                    keyType = keyType.includes('contract') ? 'address' : keyType;
+                    const encoded = abiCoder.encode([keyType, 'uint256'], [property, target.storageSlot_jslib.toHexString()]);
                     const keccak = ethers.keccak256(encoded);
                     returnProxy.storageSlot_jslib = BigNumber.from(keccak);
                     returnProxy.currentType_jslib = currentType.value;
+                    // console.debug('in mapping', keyType, property, target.storageSlot_jslib);
                 }
                 
                 const returnType = types[returnProxy.currentType_jslib];
@@ -56,8 +56,16 @@ async function makeHandler(contractAddress, blockNumber = 'latest') {
                     // Return is a thenable object. This function itself cannot be async since it
                     // chains many calls together and needs to avoid invoking its own .then
                     console.log('Retrieving storage slot:', returnProxy.storageSlot_jslib.toHexString());
+                    // TODO: handle when its an array having multiple slots
                     return provider.getStorageAt(contractAddress, returnProxy.storageSlot_jslib, blockNumber)
-                            .then(valueAtSlot => getStorageBytes(valueAtSlot, slotOffset, returnType.numberOfBytes));
+                            .then(valueAtSlot => {
+                                const result = getStorageBytes(valueAtSlot, slotOffset, returnType.numberOfBytes);
+                                if (returnType.label === 'bool') {
+                                    return result === '0x01';
+                                } else {
+                                    return result;
+                                }
+                            });
                 }
                 return returnProxy;
             }
@@ -81,7 +89,7 @@ function getStorageBytes(data, start, size) {
 
 async function storageTest() {
     
-    const handler = await makeHandler(BEANSTALK);
+    const handler = await makeHandler(BEANSTALK, 19235371);
     // Transform all storage variables from an array into an object, such that labels are extracted as keys,
     // and the underlying object is using a custom proxy.
     const beanstalk = {};
@@ -100,10 +108,23 @@ async function storageTest() {
     console.log('season: ', seasonTimestamp, seasonNumber, sunriseBlock);
 
     // Mapping (recent sow as example)
-    const account = '0x4Fea3B55ac16b67c279A042d10C0B7e81dE9c869';
+    const sower = '0x4Fea3B55ac16b67c279A042d10C0B7e81dE9c869';
     const index = '949411235551363';
-    const pods = await beanstalk.s.a[account].field.plots[index];
+    const pods = await beanstalk.s.a[sower].field.plots[index];
     console.log('pods: ', pods);
+
+    // Double mappings
+    const unripeHolder = '0xbcc44956d70536bed17c146a4d9e66261bb701dd';
+    const claimedURBean = await beanstalk.s.unripeClaimed[UNRIPE_BEAN][unripeHolder];
+    const claimedURLP = await beanstalk.s.unripeClaimed[UNRIPE_LP][unripeHolder];
+    console.log('claimedUnripe?', claimedURBean, claimedURLP);
+
+    const internalBalanceHolder = '0xDE3E4d173f754704a763D39e1Dcf0a90c37ec7F0';
+    const internalBeans = await beanstalk.s.internalTokenBalance[internalBalanceHolder][BEAN];
+    console.log('internal balance:', internalBeans);
+
+    // TODO: arrays - could require multiple slots, and parsing into the array
+    // TODO: dynamic arrays
 
 }
 storageTest();
